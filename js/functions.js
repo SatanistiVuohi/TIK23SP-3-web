22baec19e00a4556e28cf19ade13bf1e5de84210
jennioksman <c3okje00@students.oamk.fi>
2024-04-23 12:17:06 +0300

    Tulokset sivulle lisätty yhteispisteenlasku

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,21 @@
-const correctCount_1 = sessionStorage.getItem('correctCount_1');
-document.querySelector('#correctCount_1').textContent = "Yhteen- ja vähennyslaskuja 0-20 oikeat vastaukset : "+correctCount_1;
+const correctCount_1 = parseInt(sessionStorage.getItem('correctCount_1'))
+document.querySelector('#correctCount_1').textContent = "Yhteen- ja vähennyslaskuja 0-20 oikeat vastaukset : "+correctCount_1
 
-const correctCount_2 = sessionStorage.getItem('correctCount_2');
-document.querySelector('#correctCount_2').textContent = "Yhteen- ja vähennyslaskuja 0-100 oikeat vastaukset : "+correctCount_2;
+const correctCount_2 = parseInt(sessionStorage.getItem('correctCount_2'))
+document.querySelector('#correctCount_2').textContent = "Yhteen- ja vähennyslaskuja 0-100 oikeat vastaukset : "+correctCount_2
 
-const points2 = sessionStorage.getItem('correctCount_3');
-document.querySelector('#correctCount_3').textContent = "Kertolaskut 1-5, 10 : "+points2;
+const points2 = parseInt(sessionStorage.getItem('correctCount_3'))
+document.querySelector('#correctCount_3').textContent = "Kertolaskut 1-5, 10 : "+points2
 
-const correctCount_4 = sessionStorage.getItem('correctCount_4');
-document.querySelector('#correctCount_4').textContent = "Jakolaskut 1-5, 10 : "+correctCount_4;
+const correctCount_4 = parseInt(sessionStorage.getItem('correctCount_4'))
+document.querySelector('#correctCount_4').textContent = "Jakolaskut 1-5, 10 : "+correctCount_4
 
-const points_1 = sessionStorage.getItem('correctCount_5');
-document.querySelector('#correctCount_5').textContent = "Kellonajat : "+points_1;
+const points_1 = parseInt(sessionStorage.getItem('correctCount_5'))
+document.querySelector('#correctCount_5').textContent = "Kellonajat : "+points_1
 
-const points_2 = sessionStorage.getItem('correctCount_6');
-document.querySelector('#correctCount_6').textContent = "Mittayksiköt : "+points_2;
+const points_2 = parseInt(sessionStorage.getItem('correctCount_6'))
+document.querySelector('#correctCount_6').textContent = "Mittayksiköt : "+points_2
+
+const totalScore = correctCount_1 + correctCount_2 + points2 + correctCount_4 + points_1 + points_2
+document.querySelector('#total').innerHTML = "Yhteipisteesi: " + totalScore
 
